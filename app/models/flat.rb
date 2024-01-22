642593ffd0022347e3fc04310254ad061c873023
Erina Ho <erina.hou@gmail.com>
2024-01-22 21:00:32 +1100

    Create reviews model, controller

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,4 +1,6 @@
 class Flat < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+
   validates :name, presence: true
   validates :address, presence: true
   validates :price_per_night, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_22_054857) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_22_095231) do
   create_table "flats", force: :cascade do |t|
     t.string "name"
     t.string "address"
@@ -21,4 +21,13 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_22_054857) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.text "content"
+    t.integer "flat_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["flat_id"], name: "index_reviews_on_flat_id"
+  end
+
+  add_foreign_key "reviews", "flats"
 end
