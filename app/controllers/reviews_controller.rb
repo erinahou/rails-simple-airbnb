4aba637bdb3d2da90c6baf87eb4e30e44f7be1d9
Erina Ho <erina.hou@gmail.com>
2024-01-23 07:55:04 +1100

    to get reviews to appear on show pages

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,8 +1,27 @@
 class ReviewsController < ApplicationController
+  before_action :flat_find, only: %i[new create]
+
   def new
     @review = Review.new
   end
 
   def create
+    @review = Review.new(review_params)
+    @review.flat_id = @flat.id
+    if @review.save
+      redirect_to flat_path(@flat)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def flat_find
+    @flat = Flat.find(params[:flat_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:content)
   end
 end
